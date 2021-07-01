a3cabb6459b79f2698e89346e0262322416ee451
Hugo ROUECHE <hroueche@demotz.local>
2021-07-01 16:04:21 +0200

    [+] added delete and edit options

--- src/pages/my-lessons.js
+++ src/pages/my-lessons.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import changeDate from '../components/change-date';
+import changeDuration from '../components/change-duration';
 import axios from 'axios';
+import Modal from 'react-modal';
+import EditLessons from './edit-lesson';
 import { useSelector } from "react-redux"
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 export default function MyLessons() {
     const user = useSelector(state => state.user.userInfo)
     const [lessons, setLessons] = useState([]);
     let lessonNumber = 0
-
+    const [openEdit, isEditOpen] = useState(false);
+    const [selectedLesson, setSelectedLesson] = useState('');
     useEffect(() => {
         axios.get('http://localhost:5000/api/lessons/')
             .then(response => {
@@ -17,6 +22,11 @@ export default function MyLessons() {
                 console.log(error);
             });
     }, []);
+
+    function deleteLesson(lessonId) {
+        axios.delete(`http://localhost:5000/api/lessons/${lessonId}`)
+            .then(res => console.log(res.data));
+    }
 
     const professor = (
         <div>
@@ -28,6 +38,8 @@ export default function MyLessons() {
                         <th scope="col">Durée</th>
                         <th scope="col">Classe</th>
                         <th scope="col">Elèves inscrits</th>
+                        <th>Editer</th>
+                        <th>Supprimer</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,14 +49,34 @@ export default function MyLessons() {
                             return <tr>
                                 <th>{lessonNumber}</th>
                                 <td>Leçon du {changeDate(lesson.date)}</td>
-                                <td>{lesson.duration}</td>
+                                <td>{changeDuration(lesson.duration)}</td>
                                 <td>{lesson.classe}</td>
                                 <td>0/{lesson.maxStudent}</td>
+                                <td><AiFillEdit className='clickable' onClick={() => `${isEditOpen(true)} ${setSelectedLesson(lesson._id)}`} /></td>
+                                <td><AiFillDelete className='clickable' onClick={() => { deleteLesson(lesson._id) }} /></td>
                             </tr>
                         } else return ''
                     })}
                 </tbody>
             </table>
+            <Modal isOpen={openEdit} className="modalContent" overlayClassName="modalOverlay" onRequestClose={() => isEditOpen(false)}>
+                {lessons.map(lesson => {
+                    if (lesson._id === selectedLesson) {
+                        return <EditLessons
+                            key={selectedLesson}
+                            lessonId={selectedLesson}
+                            professorId={lesson.professorId}
+                            description={lesson.description}
+                            duration={lesson.duration}
+                            subject={lesson.subject}
+                            classe={lesson.classe}
+                            maxStudent={lesson.maxStudent}
+                            date={lesson.date}
+                            registeredStudents={lesson.registeredStudents}
+                        />
+                    } else return ' '
+                })}
+            </Modal>
         </div >
     )
 
