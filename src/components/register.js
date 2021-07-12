0356b8f817fb74082ea8e9b71525c618b7a67d60
Hugo <roueche.hugo@hotmail.fr>
2021-07-12 12:06:20 +0200

    [+] register function

--- src/components/register.js
+++ src/components/register.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 export default function register(lessonId, userId) {
     axios.get(`http://localhost:5000/api/lessons/${lessonId}`)
         .then(response => {
-            if (response.data.registeredStudents.find(element => element === userId)) {
-                console.log("User already registered")
-            } else {
-                let updatedRegisteredStudents = [...response.data.registeredStudents, userId]
+            if (response.data.registeredStudents.length < response.data.maxStudent) {
+                if (response.data.registeredStudents.find(element => element === userId) === userId) {
+                    console.log("User already registered")
+                } else {
+                    let updatedRegisteredStudents = [...response.data.registeredStudents, userId]
 
-                const lesson = {
-                    registeredStudents: updatedRegisteredStudents
-                };
-                axios.put(`http://localhost:5000/api/lessons/update/${lessonId}`, lesson)
-                    .then(res => console.log(res.data));
-            }
+                    const lesson = {
+                        registeredStudents: updatedRegisteredStudents
+                    };
+                    axios.put(`http://localhost:5000/api/lessons/update/${lessonId}`, lesson)
+                        .then(res => console.log(res.data));
+                }
+            } else { return console.log('Maximum number of students reached') }
         });
 }
