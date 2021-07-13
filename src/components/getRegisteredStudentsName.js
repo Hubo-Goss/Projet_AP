80503fec9f128185c618cc3990a117c3dfabde14
Hugo ROUECHE <hroueche@demotz.local>
2021-07-13 14:15:09 +0200

    [=] file renamed

--- src/components/getRegisteredStudentsName.js
+++ src/components/getRegisteredStudentsName.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 
-export default function GetRegisteredStudentsNames(props) {
+export default function GetRegisteredStudentsName(props) {
     const registeredStudents = (props.registeredStudents)
     const [registeredStudentsName, setRegisteredStudentsName] = useState([]);
     let name = []
