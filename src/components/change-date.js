3681581be2d8e058d43add69e9cdb743a478e7f4
Hugo ROUECHE <hroueche@demotz.local>
2021-11-19 09:42:28 +0100

    [+] Comment

--- src/components/change-date.js
+++ src/components/change-date.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Change ISO-8601 format date (YYYY-MM-DDTHH:MM:SS) to this format : DD/MM/YYYY
 export default function changeDate(date) {
     let dateArray = date.split("-")
     return <span>{dateArray[2].substr(0, 2) + '/' + dateArray[1] + '/' + dateArray[0]}</span>
