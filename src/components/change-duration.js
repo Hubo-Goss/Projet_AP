66a72e81cc2812d1c4f7ac20eb98ece8e41adc6b
Hugo ROUECHE <hroueche@demotz.local>
2021-11-19 09:42:47 +0100

    [-] removed 30minutes lessons

--- src/components/change-duration.js
+++ src/components/change-duration.js
@@ -1,9 +1,6 @@
 export default function changeDuration(duration) {
     let finalDuration = ''
     switch (duration) {
-        case 30:
-            finalDuration = '00h30'
-            break;
         case 60:
             finalDuration = '01h00'
             break;
