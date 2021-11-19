770e1851af5e90bf966dc22a08d4a998bfff16fc
Hugo ROUECHE <hroueche@demotz.local>
2021-11-19 09:41:06 +0100

    [+] added delete route

--- backend/routes/subjects.js
+++ backend/routes/subjects.js
@@ -8,12 +8,19 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res, next) => needsRole(req, res, next, 'Admin'), (req, res) => {
+router.route('/add').post((req, res, next) => needsRole(req, res, next, ['Admin']), (req, res) => {
     const subjectName = req.body.subjectName;
 
     const newSubject = new Subject({ subjectName });
     newSubject.save()
         .then(() => res.json('Subject added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res, next) => needsRole(req, res, next, ['Admin']), (req, res) => {
+    console.log("c'est rentré")
+    Subject.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Subject deleted.'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
