ca75c9f21c531704467e7ae72f9d743df44c1cdf
Hugo ROUECHE <hroueche@demotz.local>
2021-11-19 10:14:10 +0100

    [+] replace by fullcalendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Modal from 'react-modal';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import frLocale from '@fullcalendar/core/locales/fr';
 import { useSelector } from 'react-redux';
 import changeDate from '../components/change-date';
 import changeDuration from '../components/change-duration';
 import register from '../components/register';
-import SubjectsList from '../components/subjects-list.js';
-
-Modal.setAppElement('#root')
+import changeBgColor from '../components/change-bg-color';
+// import { Tooltip } from 'bootstrap';
 
 export default function Calendar() {
     const user = useSelector(state => state.user.userInfo)
@@ -16,7 +17,17 @@ export default function Calendar() {
     const [open, isOpen] = useState(false);
     const [selectedLesson, setSelectedLesson] = useState('');
     const [correspondingProfessor, setCorrespondingProfessor] = useState('');
-    const [subjectFilter, setSubjectFilter] = useState('DEFAULT');
+
+    const events = lessons.map(lesson => {
+        const event = {}
+        event.backgroundColor = changeBgColor(lesson)
+        event.borderColor = 'black'
+        event.title = `${lesson.subject}`
+        event.start = lesson.date
+        event.end = new Date(new Date(lesson.date).setTime(new Date(lesson.date).getTime() + lesson.duration * 60 * 1000))
+        event.extendedProps = { _id: lesson._id, professorId: lesson.professorId, numberOfStudents: `${lesson.registeredStudents.length}/${lesson.maxStudent}` }
+        return event
+    })
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/lessons/')
@@ -36,81 +47,69 @@ export default function Calendar() {
             });
     }, []);
 
-    function lessonDisplaySize(duration) {
-        switch (duration) {
-            case 30: return 'lessonSmallFormat thirtyminutesSize';
-            case 60: return 'lessonSmallFormat oneHourSize';
-            case 90: return 'lessonSmallFormat oneAndHalfHourSize';
-            case 120: return 'lessonSmallFormat twoHoursSize';
-            default: console.log('trop long');
-        }
-    }
-
-    function onChangeSubject(subject) {
-        setSubjectFilter(subject)
-    }
-
     if (!user) return null
 
     let registerButton = <button onClick={() => { register(selectedLesson, user._id) }}>S'inscrire</button>
 
-    return (
-        <div>
-            <p>Vous êtes sur la page des AP, voici les leçons disponibles :</p>
-            <div>Filtrer par matière : </div><div className="smallSelect"><SubjectsList className="smallSelect" onChange={onChangeSubject} /></div>
-            <div>{lessons.map(lesson => {
-                if (subjectFilter === "DEFAULT") {
-                    //Afficher toutes les leçons (en fonction du user)
-                    if ((lesson.classe === user.classe || user.role === "Admin" || user.role === "Professor")) {
-                        return <div className={`${lessonDisplaySize(lesson.duration)} ${lesson.subject}`} onClick={() => (`${isOpen(true)} ${setSelectedLesson(lesson._id)} ${setCorrespondingProfessor(lesson.professorId)}`)} key={lesson._id}>
-                            <div className={"subjectSmallFormat"}>{lesson.subject}</div>
-                            <span className="professorNameSmallFormat">{professors.map(professor => {
-                                if (lesson.professorId === professor._id) {
-                                    return <div key={professor._id}>{professor.firstName + ' ' + professor.lastName}</div>
-                                } else return ' '
-                            })}</span>
-                            <span className="creationDateSmallFormat">Créé le {changeDate(lesson.createdAt)}</span>
-                        </div>
-                    } else return ''
-                } else {
-                    //Affiche seulement les leçons dont la metière correspond au filtre actif
-                    if ((lesson.classe === user.classe || user.role === "Admin" || user.role === "Professor") && lesson.subject === subjectFilter) {
-                        return <div className={`${lessonDisplaySize(lesson.duration)} ${lesson.subject}`} onClick={() => (`${isOpen(true)} ${setSelectedLesson(lesson._id)} ${setCorrespondingProfessor(lesson.professorId)}`)} key={lesson._id}>
-                            <div className={"subjectSmallFormat"}>{lesson.subject}</div>
-                            <span className="professorNameSmallFormat">{professors.map(professor => {
-                                if (lesson.professorId === professor._id) {
-                                    return <div key={professor._id}>{professor.firstName + ' ' + professor.lastName}</div>
-                                } else return ' '
-                            })}</span>
-                            <span className="creationDateSmallFormat">Créé le {changeDate(lesson.createdAt)}</span>
-                        </div>
-                    } else return ''
-                }
-            }
-            )}</div>
-            <Modal isOpen={open} className="modalContent" overlayClassName="modalOverlay" onRequestClose={() => isOpen(false)}>
-                <div>{lessons.map(lesson => {
-                    if (selectedLesson === lesson._id) {
-                        return <div className={`${"lesson"} ${lesson.subject}`} key={lesson._id}>
-                            <h3 className="subject">{lesson.subject}<button className="close" onClick={() => isOpen(false)}>x</button></h3>
-                            <div><div className="professorName">Professeur : {professors.map(professor => {
-                                if (correspondingProfessor === professor._id) {
-                                    return <span key={professor._id} value={professor._id}>{professor.firstName + ' ' + professor.lastName}</span>
-                                } else return ' '
-                            })}</div>
-                                <div>Classe : {lesson.classe}</div>
-                                <div>Durée : {changeDuration(lesson.duration)}</div>
-                                <div>Elèves inscrits : {lesson.registeredStudents.length}/{lesson.maxStudent}</div>
-                                <div>Leçon prévue le {changeDate(lesson.date)}</div>
-                                <p>Contenu de la leçon : {lesson.description}</p>
-                                {user.role === "Student" || user.role === "Admin" ? registerButton : ''}
-                                <div className="creationDate">Créé le {changeDate(lesson.createdAt)}</div>
-                            </div>
-                        </div>
+    const lessonInfo = <div id="lessonInfo">{lessons.map(lesson => {
+        if (selectedLesson === lesson._id) {
+            return <div className={`${"lesson"} ${lesson.subject}`} key={lesson._id}>
+                <h3 className="subject">{lesson.subject}<button className="close" onClick={() => isOpen(false)}>x</button></h3>
+                <div><div className="professorName">Professeur : {professors.map(professor => {
+                    if (correspondingProfessor === professor._id) {
+                        return <span key={professor._id} value={professor._id}>{professor.firstName + ' ' + professor.lastName}</span>
                     } else return ' '
                 })}</div>
-            </Modal>
-        </div>
+                    <div>Classe : {lesson.classe}</div>
+                    <div>Durée : {changeDuration(lesson.duration)}</div>
+                    <div>Elèves inscrits : {lesson.registeredStudents.length}/{lesson.maxStudent}</div>
+                    <div>Leçon prévue le {changeDate(lesson.date)}</div>
+                    <p>Contenu de la leçon : {lesson.description}</p>
+                    {user.role === "Student" || user.role === "Admin" ? registerButton : ''}
+                    <div className="creationDate">Créé le {changeDate(lesson.createdAt)}</div>
+                </div>
+            </div>
+        } else return ' '
+    })}</div>
+
+    return (
+        <div>
+            <FullCalendar
+                locale={frLocale}
+                plugins={[timeGridPlugin]}
+                titleFormat={{ year: 'numeric', month: 'long', day: 'numeric' }}
+                headerToolbar={{ left: '', center: 'title', right: 'prev,today,next' }}
+                initialView="timeGridWeek"
+                allDaySlot={false}
+                weekends={false}
+                slotMinTime="08:00:00"
+                slotMaxTime="19:00:00"
+                height="auto"
+                events={events}
+                eventClick={function (info) {
+                    setSelectedLesson(info.event._def.extendedProps._id)
+                    setCorrespondingProfessor(info.event._def.extendedProps.professorId)
+                    isOpen(true)
+                }}
+                eventContent={function (arg) {
+                    let title = document.createElement('div')
+                    let numberOfStudents = document.createElement('div')
+
+                    title.innerHTML = arg.event.title
+                    numberOfStudents.innerHTML = arg.event.extendedProps.numberOfStudents
+
+                    let arrayOfDomNodes = [title, numberOfStudents]
+                    return { domNodes: arrayOfDomNodes }
+                }
+                }
+            // slotLabelContent={
+            //     function (level) {
+            //         var hours = ["08h05", "09h00", "09h55", "11h05", "12h00", "13h10", "14h05", "14h00", "15h10", "16h05", "17h00", "17h55"]
+            //         return hours[level]
+            //     }
+            // }
+            />
+            {open === true ? lessonInfo : null}
+        </div >
     )
 }
-
